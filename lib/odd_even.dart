87ca6b5fe7cf4b854da6393968024a4115247499
GungDeRaka <aswinraka4@gmail.com>
2022-11-02 12:23:09 +0800

    a FormatException on main.dart

--- lib/odd_even.dart
+++ lib/odd_even.dart
@@ -3,13 +3,14 @@ import 'dart:io';
 void main() {
   String? s = stdin.readLineSync();
   int angka = 0;
-  try{
-    if(s != null){
+  try {
+    if (s != null) {
       angka = int.parse(s);
     }
-    print("$angka merupakan ${(angka % 2 == 0)? "angka Genap": "angka Ganjil"}");
-  }on Exception {print("Inputan harus berupa angka!");
-  main();
+    print(
+        "$angka merupakan ${(angka % 2 == 0) ? "angka Genap" : "angka Ganjil"}");
+  } on Exception {
+    print("Inputan harus berupa angka!");
+    main();
   }
-
 }
