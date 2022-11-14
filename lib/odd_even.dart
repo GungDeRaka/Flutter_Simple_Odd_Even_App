447dcd441e391725ca551724816fa87d613f25f3
GungDeRaka <aswinraka4@gmail.com>
2022-11-14 21:48:41 +0800

    ngedit

--- lib/odd_even.dart
+++ lib/odd_even.dart
@@ -1,14 +1,9 @@
 import 'dart:io';
 
 void main() {
-  String? s = stdin.readLineSync();
-  int angka = 0;
   try {
-    if (s != null) {
-      angka = int.parse(s);
-    }
-    print(
-        "$angka merupakan ${(angka % 2 == 0) ? "angka Genap" : "angka Ganjil"}");
+  int angka = int.parse(stdin.readLineSync().toString());
+    print("$angka merupakan ${(angka % 2 == 0) ? "angka Genap" : "angka Ganjil"}");
   } on Exception {
     print("Inputan harus berupa angka!");
     main();
